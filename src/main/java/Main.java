0f320a98383c8acdd7f21f49d09fb8ee8de5e9d1
Kseniya <kosnireva9469@gmail.com>
2023-02-24 12:36:46 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
+    private static String m = "Hello world";
+    
     public static void main (String[] args) {
-        System.out.println("Hello world");
+        System.out.println(m);
     }
 }
